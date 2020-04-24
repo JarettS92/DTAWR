054caa65f7d646968049e090eabe7386c842787b
JarettS92 <38923465+JarettS92@users.noreply.github.com>
2020-04-24 10:41:50 -0400

    added datepicker

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -24,6 +24,28 @@ window.onload = function() {
         $("#csatReminderBanner").hide();
     });
 
+    // Datepicker tool
+    var start = moment().subtract(30, 'days');
+    var end = moment();
+
+    function cb(start, end) {
+        $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+        console.log(start.format('YYYYMMDD'), end.format('YYYYMMDD'));
+    }
+    $('#reportrange').daterangepicker({
+        startDate: start,
+        endDate: end,
+        ranges: {
+            'Today': [moment(), moment()],
+            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+            'This Month': [moment().startOf('month'), moment().endOf('month')],
+            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        }
+    }, cb);
+    cb(start, end);
+
     // Hover dropdown for environment button
     // Displays list of configured envs/tokens
     // $('.nav__item').hover(
